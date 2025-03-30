55f6c4af03a589bbd43c6380211792850a3b4185
Riatre Foo <foo@riat.re>
2025-03-31 01:48:20 +0800

    Add schema.py

---
+++ gtg_eval/schema.py
@@ -0,0 +1,44 @@
+import pydantic
+import enum
+import litellm.types.llms.openai as litellm_openai
+
+
+class Game(pydantic.BaseModel):
+    id: str
+    description: str
+    answers: list[str]
+    franchise: str
+    submitted_by: str
+    release_year: str
+    metacritic_score: str
+    genre: str
+    console_platform: str
+    developer: str
+
+
+class Verdict(enum.StrEnum):
+    CORRECT = "correct"
+    INCORRECT = "incorrect"
+    SAME_FRANCHISE = "same_franchise"
+
+
+class Guess(pydantic.BaseModel):
+    answer: str
+    verdict: Verdict
+
+
+class EvaluationState(pydantic.BaseModel):
+    game: Game
+    guesses: list[Guess] = []
+    messages: list[litellm_openai.AllMessageValues] = []
+
+
+class PromptTemplate(pydantic.BaseModel):
+    initial: str
+    incorrect: str
+    same_franchise: str
+    after_1: str
+    after_2: str
+    after_3: str
+    after_4: str
+    after_5: str
